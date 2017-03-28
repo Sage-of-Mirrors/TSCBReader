324a3983fb5bfb95041ec5736cf5fd3b468bf9fe
Sage_of_Mirrors <empressglatia@gmail.com>
2017-03-27 19:40:04 -0500

    Now loads all of the data.

--- TSCBReader/src/TSCB.cs
+++ TSCBReader/src/TSCB.cs
@@ -53,6 +53,7 @@ namespace TSCBReader.src
                 UnknownFloat1 = reader.ReadSingle();
                 UnknownInt3 = reader.ReadInt32();
 
+                long sectionStartPos = reader.BaseStream.Position;
                 int firstSectionSize = reader.ReadInt32();
 
                 for (int i = 0; i < instanceCount; i++)
@@ -68,14 +69,90 @@ namespace TSCBReader.src
 
                     reader.BaseStream.Seek(curPos + 4, SeekOrigin.Begin);
                 }
+
+                reader.BaseStream.Seek(sectionStartPos + firstSectionSize, SeekOrigin.Begin);
             }
         }
 
         public class SecondSection
         {
-            public void Load(EndianBinaryReader reader)
+            public class SecondSectionInstance
             {
+                public float UnknownFloat1 { get; private set; }
+                public float UnknownFloat2 { get; private set; }
+                public float UnknownFloat3 { get; private set; }
+                public float UnknownFloat4 { get; private set; }
+                public float UnknownFloat5 { get; private set; }
+                public float UnknownFloat6 { get; private set; }
+                public float UnknownFloat7 { get; private set; }
+                public int UnknownInt1 { get; private set; }
+                public string FileName { get; private set; }
+                public int UnknownInt2 { get; private set; }
+                public int UnknownInt3 { get; private set; }
+                public int UnknownInt4 { get; private set; }
+                public List<int> VariableSectionInts { get; private set; }
 
+                private int m_stringOffset;
+                private int m_variableSectionCount;
+
+                public void Load(EndianBinaryReader reader)
+                {
+                    VariableSectionInts = new List<int>();
+
+                    UnknownFloat1 = reader.ReadSingle();
+                    UnknownFloat2 = reader.ReadSingle();
+                    UnknownFloat3 = reader.ReadSingle();
+                    UnknownFloat4 = reader.ReadSingle();
+                    UnknownFloat5 = reader.ReadSingle();
+                    UnknownFloat6 = reader.ReadSingle();
+                    UnknownFloat7 = reader.ReadSingle();
+                    UnknownInt1 = reader.ReadInt32();
+
+                    m_stringOffset = reader.PeekReadInt32();
+                    long curPos = reader.BaseStream.Position;
+                    reader.BaseStream.Seek(m_stringOffset, SeekOrigin.Current);
+                    FileName = reader.ReadStringUntil('\0');
+                    reader.BaseStream.Seek(curPos + 4, SeekOrigin.Begin);
+
+                    UnknownInt2 = reader.ReadInt32();
+                    UnknownInt3 = reader.ReadInt32();
+                    UnknownInt4 = reader.ReadInt32();
+
+                    if (UnknownInt1 == 0)
+                        return;
+
+                    m_variableSectionCount = reader.ReadInt32();
+
+                    for (int i = 0; i < m_variableSectionCount; i++)
+                        VariableSectionInts.Add(reader.ReadInt32());
+                }
+
+                public override string ToString()
+                {
+                    return $"[{ VariableSectionInts.Count }]";
+                }
+            }
+
+            public List<SecondSectionInstance> SecondSectionInstances { get; private set; }
+
+            public void Load(EndianBinaryReader reader, int sectionCount)
+            {
+                SecondSectionInstances = new List<SecondSectionInstance>();
+
+                long basePos = reader.BaseStream.Position;
+
+                for (int i = 0; i < sectionCount; i++)
+                {
+                    basePos = reader.BaseStream.Position;
+                    int instanceOffset = reader.ReadInt32();
+                    long nextInstOffset = reader.BaseStream.Position;
+
+                    reader.BaseStream.Seek(basePos + instanceOffset, SeekOrigin.Begin);
+                    SecondSectionInstance inst = new SecondSectionInstance();
+                    inst.Load(reader);
+                    SecondSectionInstances.Add(inst);
+                    reader.BaseStream.Seek(nextInstOffset, SeekOrigin.Begin);
+                }
             }
         }
 
@@ -111,7 +188,7 @@ namespace TSCBReader.src
                 FirstSectionData.Load(reader, m_firstSectionCount);
 
                 SecondSectionData = new SecondSection();
-                SecondSectionData.Load(reader);
+                SecondSectionData.Load(reader, m_secondSectionCount);
             }
         }
     }
